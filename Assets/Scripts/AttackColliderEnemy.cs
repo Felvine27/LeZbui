bb4265c2952380e897ae604663b95a5a201c91f4
Kalethras <arnaud.siquet@hotmail.com>
2019-08-07 11:07:20 +0200

    ajouter Enemy prefab plus IA

---
+++ Assets/Scripts/AttackColliderEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackColliderEnemy : MonoBehaviour
+{
+
+    private BoxCollider attackArea;
+    private bool canAttack = true;
+
+    [SerializeField]
+    private float attackCooldownTime = 3.0F;
+
+    [SerializeField]
+    private float attackAreaDuration = 2.0F;
+
+    private void Start()
+    {
+        attackArea = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        AttackMethod();
+    }
+
+    private void AttackMethod()
+    {
+        canAttack = false;
+        TriggerAttackCooldownCoroutine();
+        attackArea.enabled = true;
+        TriggerAttackTimerCoroutine();
+    }
+
+    public void TriggerAttackTimerCoroutine()
+    {
+        StartCoroutine(AttackTimerCoroutine());
+    }
+
+    private IEnumerator AttackTimerCoroutine()
+    {
+        
+        yield return new WaitForSeconds(attackAreaDuration);
+        attackArea.enabled = false;
+    }
+
+    public void TriggerAttackCooldownCoroutine()
+    {
+        StartCoroutine(AttackCooldownCoroutine());
+    }
+
+    private IEnumerator AttackCooldownCoroutine()
+    {
+        Debug.Log("Cooldown started");
+        yield return new WaitForSeconds(attackCooldownTime);
+        Debug.Log("Cooldown ended");
+        canAttack = true;
+    }
+
+    private void OnTriggerEnter(Collider enemyCollided)
+    {
+
+        if (enemyCollided.CompareTag("Enemy"))
+            Destroy(enemyCollided.gameObject);
+
+    }
+}
---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+
+    private float Distance;
+
+    private float DistanceBase;
+    private Vector3 basePositions;
+
+    public Transform Target;
+
+    public float chaseRange = 10;
+
+    public float attackRange = 1.0f;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    //private Animation animations;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //animations = gameObject.GetComponent<Animation>();
+        basePositions = transform.position;
+    }
+
+
+
+    void Update()
+    {
+
+        Target = GameObject.Find("Player").transform;
+
+        Distance = Vector3.Distance(Target.position, transform.position);
+
+        DistanceBase = Vector3.Distance(basePositions, transform.position);
+
+        if (Distance < chaseRange && Distance > attackRange)
+        {
+            chase();
+        }
+
+        if (Distance < attackRange)
+        {
+            attack();
+        }
+
+        if (Distance > chaseRange && DistanceBase > 1)
+        {
+            BackBase();
+        }
+
+    }
+
+    void chase()
+    {
+        agent.destination = Target.position;
+    }
+
+    void attack()
+    {        
+        agent.destination = transform.position;
+    }
+
+    public void BackBase()
+    {
+        agent.destination = basePositions;
+    }
+
+}
+
+
