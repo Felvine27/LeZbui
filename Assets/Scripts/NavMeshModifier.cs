b12c683a6a20e301b59ff6ab46b5728a21ac237b
Kalethras <arnaud.siquet@hotmail.com>
2019-08-07 11:58:30 +0200

    Ajout des scripts modifiant le NavMesh (qui ne fonctionnent pas, nique)

---
+++ Assets/Scripts/NavMeshModifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshModifier : MonoBehaviour
+{
+    public NavMeshSurface surface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void NavMeshModified()
+    {
+        surface.BuildNavMesh();
+        Debug.Log("Le NavMesh a été mis à jour");
+    }
+}
